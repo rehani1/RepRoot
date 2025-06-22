394c7cdb815e80d3fa9ee89c52e76664ee43d228
Sebastian Baldeon <fjc8mp@virginia.edu>
2025-06-22 15:58:36 -0400

    Added Supabase

---
+++ RepRootRN/lib/supabase.js
@@ -0,0 +1,5 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://voop1ktpzywihsffzuus.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZvb3Bsa3Rwenl3aWhzZmZ6dXVzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA2MjA5NDUsImV4cCI6MjA2NjE5Njk0NX0.fzdMM6F7suRiLDBFmKfHwp2cZr-6FjeGZ_-SHxAdQUs'; /*  */
+export const supabase = createClient(supabaseUrl, supabaseKey);
